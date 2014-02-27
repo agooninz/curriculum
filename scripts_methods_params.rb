c6e310f15e5b28c7f337fa6b2430847958a7ed65
John Davison <johncdavison@gmail.com>
2014-02-27 12:28:59 -0800

    updates.

--- scripts_methods_params.rb
+++ scripts_methods_params.rb
@@ -1,19 +1,21 @@
+# https://github.com/rubyonrailstutor/curriculum/blob/master/scripts_methods_params.rb
 # defining and invoking methods, using a script file
 
 def display_tweet
-  tweet = "i like to eat random stuff cause i'm a hipster"
+  tweet = "cronuts reflect firey confection"
   p "my thoughts are poetry; #{tweet}"
 end
 
 # Return values, explicit and implicit
 # What does it mean to 'return' something?
-def example_method
+
+def fred
   name = "sarah"
   p name
   "fred"
 end
-  
-def example_method2
+
+def sarah
   name = "sarah"
   p name
   return name
@@ -30,7 +32,7 @@ end
 
 # Ruby and parethesis, or lack there of
 
-def do_a_dance dance1, dance2 
+def do_a_dance dance1, dance2
   p dance1
   dance2
 end
@@ -48,11 +50,11 @@ def make_a_decision_based_on_input(condition)
   if condition == true
     p "the condition was true"
   else
-    p "the conditioin was not true"
+    p "the condition was not true"
   end
 end
 
-def make_a_different_decision(condition)
+def make_a_different_decision(condition = false)
   return "fubar" unless condition
 end
 
